7bb22201a9e33812e9d53b10923bf8edb0ec9069
Valeria <valerijamiller2001@gmail.com>
2024-03-16 15:12:47 +0100

    Portfolio

--- src/js/script.js
+++ src/js/script.js
@@ -12,4 +12,29 @@ VANTA.NET({
     points: 18.00,
     maxDistance: 26.00,
     spacing: 14.00
-})
+});
+
+
+/* Гамбургер */
+const hamburger = document.querySelector('.promo_hamburger'),
+    menu = document.querySelector('.menu'),
+    closeElem = document.querySelector('.menu_close'),
+    menuList = document.querySelector('.menu_list'),
+    items = menuList.querySelectorAll('.menu_link');
+
+hamburger.addEventListener('click', () => {
+    document.body.classList.toggle('_lock');
+    menu.classList.add('active');
+});
+
+closeElem.addEventListener('click', () => {
+    menu.classList.remove('active');
+    document.body.classList.remove('_lock');
+});
+
+for (let i=0; i< items.length; i++){
+    items[i].addEventListener('click', () => {
+        menu.classList.remove('active');
+        document.body.classList.remove('_lock');
+    });
+}
