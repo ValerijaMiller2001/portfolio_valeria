b4273f2d3a7ca3dfd1fbb0e3b8ce1fb422bcb0a8
Valeria <valerijamiller2001@gmail.com>
2024-04-26 18:20:41 +0200

    Slider

--- docs/js/script.js
+++ docs/js/script.js
@@ -1,3 +1,5 @@
+// const { off } = require("gulp");
+
 document.addEventListener('DOMContentLoaded', () => {
     //Главный фон
     VANTA.NET({
@@ -134,8 +136,131 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     }
 
+    //Portfolio
+
+    const portfolioSlider = document.querySelector('.portfolio_slider');
+    const portfolioWrapper = document.querySelector('.portfolio_wrapper');
+    const portfolioItem = document.querySelectorAll('.portfolio_item');
+    const portfolioBlock = document.querySelector('.portfolio_block');
+    const portfolioPrew = document.querySelector('.portfolio_prew');
+    const portfolioNext = document.querySelector('.portfolio_next');
+    const portfolioWidth = window.getComputedStyle(portfolioWrapper).width;
+    let slideIndex = 1;
+    let offset = 0;
+    let touchStartX = 0;
+    let touchEndX = 0;
+
+    if (window.screen.width > 768) {
+        portfolioBlock.style.display = 'grid';
+    } else {
+        portfolioBlock.style.width = 100 * portfolioItem.length + '%';
+        portfolioBlock.style.display = 'flex';
+        portfolioBlock.style.transition = '0.5s all';
+
+        portfolioWrapper.style.overflow = 'hidden';
+        portfolioItem.forEach(slide => {
+            slide.style.width = portfolioWidth;
+        });
+
+        portfolioSlider.style.position = 'relative';
+        const indicators = document.createElement('ol');
+        const dots = [];
+        indicators.classList.add('portfolio_indicators');
+        portfolioSlider.append(indicators);
+
+        for (let i = 0; i < portfolioItem.length; i++) {
+            const dot = document.createElement('li');
+            dot.setAttribute('data-slide-to', i + 1);
+            dot.classList.add('portfolio_dot');
+            if (i == 0) {
+                dot.style.opacity = 1;
+            }
+            indicators.append(dot);
+            dots.push(dot);
+        }
+
+        function handleSwipe() {
+            const swipeLength = touchEndX - touchStartX;
+            if (Math.abs(swipeLength) > 50) {
+                if (swipeLength < 0) {
+                    nextSlide();
+                } else {
+                    prewSlide();
+                }
+            }
+        }
+
+        function showOpacityDots() {
+            dots.forEach(dot => {
+                dot.style.opacity = '0.5';
+            });
+            dots[slideIndex - 1].style.opacity = '1';
+        }
+
+        function nextSlide() {
+            if (offset == +portfolioWidth.slice(0, portfolioWidth.length - 2) * (portfolioItem.length - 1)) {
+                offset = 0;
+            } else {
+                offset += +portfolioWidth.slice(0, portfolioWidth.length - 2);
+            }
+
+            portfolioBlock.style.transform = `translateX(-${offset}px)`;
+            if (slideIndex == portfolioItem.length) {
+                slideIndex = 1;
+            } else {
+                slideIndex++;
+            }
+            showOpacityDots();
+        }
+
+        function prewSlide() {
+            if (offset == 0) {
+                offset = +portfolioWidth.slice(0, portfolioWidth.length - 2) * (portfolioItem.length - 1)
+            } else {
+                offset -= +portfolioWidth.slice(0, portfolioWidth.length - 2);
+            }
+
+            portfolioBlock.style.transform = `translateX(-${offset}px)`;
+            if (slideIndex == 1) {
+                slideIndex = portfolioItem.length;
+            } else {
+                slideIndex--;
+            }
+            showOpacityDots();
+        }
+
+        portfolioWrapper.addEventListener('touchstart', (e) => {
+            touchStartX = e.touches[0].clientX;
+        });
+        portfolioWrapper.addEventListener('touchmove', (e) => {
+            touchEndX = e.touches[0].clientX;
+        });
+        portfolioWrapper.addEventListener('touchend', () => {
+            handleSwipe();
+        });
+
+        portfolioNext.addEventListener('click', () => {
+            nextSlide();
+        });
+
+        portfolioPrew.addEventListener('click', () => {
+            prewSlide();
+        });
+
+        dots.forEach(dot => {
+            dot.addEventListener('click', (e) => {
+                const slideTo = e.target.getAttribute('data-slide-to');
+                slideIndex = slideTo;
+                offset = +portfolioWidth.slice(0, portfolioWidth.length - 2) * (slideTo - 1);
+                portfolioBlock.style.transform = `translateX(-${offset}px)`;
+                showOpacityDots();
+            });
+        });
+
+    }
 
 
-})
+
+});
 
 
